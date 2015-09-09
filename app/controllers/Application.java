38f9949e1a7dd0f907adea6e15f7ada8cd6626c0
superleo <superleo.peking@gmail.com>
2015-09-09 20:27:02 +0800

    First import

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,33 +1,12 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import constants.Constants;
-import constants.Messages;
-import models.Craft;
-import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
-
-import java.util.List;
 
 public class Application extends Controller {
 
     public Result index() {
         return ok(views.html.index.render());
-    }
-
-    public Result findAll() {
-        ObjectNode result = Json.newObject();
-        try {
-            List<Craft> list = Craft.findAll();
-            result.put(Constants.CODE, Constants.SUCCESS);
-            result.replace(Constants.DATAS, Json.toJson(list));
-        } catch (Exception e) {
-            result.put(Constants.CODE, Constants.ERROR);
-            result.put(Constants.MESSAGE, Messages.QUERY_ERROR);
-        }
-        return ok(result);
     }
 
 }
---
+++ app/controllers/Auctions.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import constants.Constants;
+import constants.Messages;
+import models.Craft;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.List;
+
+public class Auctions extends Controller {
+
+    public Result findAll() {
+        ObjectNode result = Json.newObject();
+        try {
+            List<Craft> list = Craft.findAll();
+            result.put(Constants.CODE, Constants.SUCCESS);
+            result.replace(Constants.DATAS, Json.toJson(list));
+        } catch (Exception e) {
+            result.put(Constants.CODE, Constants.ERROR);
+            result.put(Constants.MESSAGE, Messages.QUERY_ERROR);
+        }
+        return ok(result);
+    }
+
+}
